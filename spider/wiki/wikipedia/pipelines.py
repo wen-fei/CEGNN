bae8f989ba1a0272dd04b07c1f6e8ceb5ebae8d4
wen-fei <282699766@qq.com>
2020-01-12 21:55:29 +0800

    insert concept image url into mysql

--- spider/wiki/wikipedia/pipelines.py
+++ spider/wiki/wikipedia/pipelines.py
@@ -4,24 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo as pymongo
-
-import settings
+from util.CUITool import CUITool
 
 
 class WikipediaPipeline(object):
     def __init__(self):
-        # 获取数据库连接信息
-        host = settings['MONGODB_HOST']
-        port = settings['MONGODB_PORT']
-        dbname = settings['MONGODB_DBNAME']
-        client = pymongo.MongoClient(host=host, port=port)
-
-        # 定义数据库
-        db = client[dbname]
-        self.table = db[settings['MONGODB_TABLE']]
+        self.db = CUITool()
+        self.data_list = []
 
     def process_item(self, item, spider):
-        quote_info = dict(item)
-        self.table.insert(quote_info)
+        sql = "INSERT INTO mrconimg(concept, url) VALUES (%s,%s)"
+        if len(self.data_list) == 2000:
+            self.db.insert_batch(self.data_list, sql)
+            self.data_list = []
+        else:
+            self.data_list.append((item['concept'], item['url']))
         return item
