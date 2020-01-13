de236a9452de2d3f8c9a91085ad1a88ea62bdb21
wen-fei <282699766@qq.com>
2020-01-13 21:35:32 +0800

    DownImagePipeline

--- spider/wiki/wikipedia/pipelines.py
+++ spider/wiki/wikipedia/pipelines.py
@@ -4,6 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+
 from .items import ImageItem, PageItem
 from .settings import BATCH_SIZE
 from .util.CUITool import CUITool
@@ -31,3 +34,17 @@ class WikipediaPipeline(object):
             else:
                 self.data_list_page.append((item["cui"], item["pageid"], item["title"], item["wikitext"]))
         return item
+
+
+# TODO, 下载图片，等最后再下载也行，单独起一个爬虫
+class DownImagePipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield Request(image_url, meta={'item': item, 'index': item['image_urls'].index(image_url)})
+
+    def file_path(self, request, response=None, info=None):
+        item = request.meta['item']  # 通过上面的meta传递过来item
+        index = request.meta['index']
+        img_name = item['cui'] + "." + request.url.split('/')[-1].split('.')[-1]
+        down_file_name = u'full/{0}'.format(img_name)
+        return down_file_name
