36149b46b8098620fd7a7c53e33808ba1c2f8186
wen-fei <282699766@qq.com>
2020-01-13 15:58:24 +0800

    add log file setting

--- spider/wiki/wikipedia/settings.py
+++ spider/wiki/wikipedia/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'wikipedia.spiders'
 LOG_LEVEL = 'DEBUG'
 to_day = datetime.datetime.now()
 log_file_path = 'log/scrapy_{}_{}_{}.log'.format(to_day.year, to_day.month, to_day.day)
-# LOG_FILE = log_file_path
+LOG_FILE = log_file_path
 
 # middleware
 DOWNLOADER_MIDDLEWARES = {
@@ -64,3 +64,5 @@ USER = "root"
 PASSWORD = "123456"
 DATABASE_NAME = "umls_subset"
 BATCH_SIZE = 2000  # batch sql size
+
+LOG_LEVEL= 'INFO'
