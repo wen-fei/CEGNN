c3d4cf31f063053d758c576f02e47babd498648c
wen-fei <282699766@qq.com>
2020-01-13 16:00:58 +0800

    add py run method

---
+++ spider/wiki/wikipedia/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:TenYun
+@time: 2020/1/13
+@contact: tenyun.zhang.cs@gmail.com
+"""
+
+from scrapy import cmdline
+cmdline.execute("scrapy crawl wiki".split())
