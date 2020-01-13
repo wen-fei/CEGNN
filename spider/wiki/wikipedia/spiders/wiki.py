6c6d34dfb7e40f253ad2abcd9e7bdef7e6d2de60
wen-fei <282699766@qq.com>
2020-01-13 16:01:14 +0800

    flash text

--- spider/wiki/wikipedia/spiders/wiki.py
+++ spider/wiki/wikipedia/spiders/wiki.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+import re
 from urllib.parse import urlencode
 import scrapy
 from scrapy.http import Request
-from items import PageItem, ImageItem
-from settings import HEADERS
-from util.CUITool import CUITool
+from ..items import PageItem, ImageItem
+from ..settings import HEADERS
+from ..util.CUITool import CUITool
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,7 @@ class WikiSpider(scrapy.Spider):
     allowed_domains = ["en.wikipedia.org"]
     wiki_base = "https://en.wikipedia.org/wiki/"
     api_url = "https://en.wikipedia.org/w/api.php"
-    start_url = [api_url]
+    start_urls = [api_url]
     meta_proxy = "http://127.0.0.1:1080"
     visited = set()
 
@@ -42,9 +43,9 @@ class WikiSpider(scrapy.Spider):
         # TODO 考虑可扩展性，因为不只一个UMLS数据库
         # cui 最大G4551440
         for i in range(1, 2277):
-            cuis = ("G" + str(i).zfill(7) for i in range(i, i + 2000))
+            cuis = [tuple(["C" + str(i).zfill(7)]) for i in range(1, 1 + 2000)]
             concept_def = db.query_batch(cuis)
-            for concept, key in concept_def:
+            for concept, key in concept_def.items():
                 url = self.start_urls[0] + "?" + urlencode(self.querystring) + "&srsearch=" + key
                 yield Request(url,
                               callback=self.parse,
@@ -73,25 +74,38 @@ class WikiSpider(scrapy.Spider):
 
     def sub_parse(self, response):
         res = json.loads(response.body_as_unicode())
-        item = PageItem()
-        item["cui"] = response.meta["concept"]
-        item["title"] = res["title"]
-        item["pageid"] = res["pageid"]
-        # TODO,得到的只是图片名字，还不是url, 根据图片名字去找对应的图片URL
-        img_api = "https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=Image:"
-        img_urls = []
-        for img_name in res["images"]:
-            yield Request(img_api + img_name,
-                          callback=self.img_parse,
-                          headers=HEADERS,
-                          meta={'proxy': self.meta_proxy, 'concept': response.meta["concept"]}
-                          )
-        item["images"] = img_urls
-        item["wikitext"] = res["wikitext"]
-        yield item
+        if res:
+            item = PageItem()
+            item["cui"] = response.meta["concept"]
+            item["title"] = res["parse"]["title"]
+            item["pageid"] = res["parse"]["pageid"]
+            # TODO,得到的只是图片名字，还不是url, 根据图片名字去找对应的图片URL
+            img_api = "https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=Image:"
+            img_urls = []
+            for img_name in res["parse"]["images"]:
+                yield Request(img_api + img_name,
+                              callback=self.img_parse,
+                              headers=HEADERS,
+                              meta={'proxy': self.meta_proxy, 'concept': response.meta["concept"]}
+                              )
+            item["images"] = img_urls
+            wikitext = res["parse"]["wikitext"]
+            item["wikitext"] = self.clean_text(wikitext)
+            yield item
 
     def img_parse(self, response):
+        text = response.body_as_unicode()
         item = ImageItem()
-        item["cui"] = response.meta["concept"]
-        item["url"] = response["query"]["pages"]["-1"]["imageinfo"]["url"]
-        return item
+        if text:
+            res = json.loads(text)
+            item["cui"] = response.meta["concept"]
+            item["url"] = res["query"]["pages"]["-1"]["imageinfo"][0]["url"]
+            yield item
+
+    def clean_text(self, wikitext):
+        wikitext = self.removeNonWordChar(wikitext)
+        return wikitext
+
+    # remove non-word chars
+    def removeNonWordChar(inputString):
+        return re.sub(r"[^\w]", "", inputString);  # non [a-zA-Z0-9_]
