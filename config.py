3f748fc11b9b9762b4ce7ac8e07bed03c99ea21c
wen-fei <282699766@qq.com>
2020-01-21 23:25:59 +0800

    add cuis all file

--- config.py
+++ config.py
@@ -12,12 +12,14 @@ today = datetime.date.today()
 
 
 class DefaultConfig(object):
+
     """ default parameters and settings """
     path = os.path.dirname(os.path.abspath(__file__))
     words_save = path + "/materials/words.dat"
     cuis_save = path + "/materials/cuis.dat"
     word2cui = path + "/materials/word2cui.dat"
     vocabulary_store = path + "/materials/vocabulary_store.dat"
+    cuis_all = path + "/materials/cuis_all.csv"
 
     pred_PubMed_vector = path + "/materials/PubMed-shuffle-win-30.bin"
     word_embeddings = path + "/materials/PubMed_extracted.pl"
