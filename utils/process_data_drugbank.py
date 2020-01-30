2fcc60ba9cadaeacc55c58df8b601d7d7289614a
wen-fei <282699766@qq.com>
2020-01-30 13:20:48 +0800

    fix some bugs and save to csv file

--- utils/process_data_drugbank.py
+++ utils/process_data_drugbank.py
@@ -8,6 +8,7 @@
 
 import lxml.etree as ET
 from tqdm import tqdm
+import numpy as np
 
 drugbank_path = "G:\\CEGNN\\materials\\drugbank_all_full_database\\drugbank.xml"
 drugbank = ET.parse(drugbank_path)
@@ -16,15 +17,31 @@ root = drugbank.getroot()
 # for node in nodes:
 #     print(node.text)
 ns = {'db': 'http://www.drugbank.ca'}
+drug_content = []
+drug_network = []
+
 for drug in tqdm(root.xpath("db:drug[db:groups/db:group='approved']", namespaces=ns)):
     drugName = drug.find("db:name", ns).text
     drugbank_id = drug.find("db:drugbank-id[@primary='true']", ns).text
     drugDescription = drug.find("db:description", ns).text
-    state = drug.find("db:state", ns).text
-    drug_interactions = drug.findall("db:drug-interactions/db:drug-interaction/db:drugbank-id", ns)
-    interactions = list(set(map(lambda x: x.text, drug_interactions)))
-    for id in interactions:
-        entity1 = drugbank_id
-        entity2 = id
-        # TODO write to csv
-        pass
+    if drugDescription is not None:
+        drugDescription = drugDescription.replace("\n", "")
+        drugDescription = drugDescription.replace("\r", "")
+    state = drug.find("db:state", ns)
+    if state is not None:
+        state = state.text
+    else:
+        state = "None"
+    drug_interactions = drug.xpath("db:drug-interactions/db:drug-interaction", namespaces=ns)
+    drug_content.append([drugbank_id, drugName, state, drugDescription])
+
+    for t in drug_interactions:
+        t_drugbank_id = t.find("db:drugbank-id", ns).text
+        name = t.find("db:name", ns).text
+        interaction = t.find("db:description", ns).text
+        drug_network.append([drugbank_id, t_drugbank_id, name, interaction])
+drug_content = np.array(drug_content, dtype=np.str)
+drug_network = np.array(drug_network, dtype=np.str)
+np.savetxt("drugbank.content", drug_content, fmt="%s", delimiter=',', encoding="utf-8")
+np.savetxt("drugbank.cites", drug_network, fmt="%s", delimiter=',', encoding="utf-8")
+np.savetxt("drugbank_d2d.cites", drug_network[:, [0, 1]], fmt="%s", delimiter=",", encoding="utf-8")
