c5e230f1bdce8cf423d016e4fbb76ae2d2da8fe6
wen-fei <282699766@qq.com>
2020-02-07 22:30:40 +0800

    update umls process

--- utils/process_data_umls.py
+++ utils/process_data_umls.py
@@ -6,13 +6,19 @@
 @contact: tenyun.zhang.cs@gmail.com
 """
 import pandas as pd
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
-umls_rel = pd.read_csv("../materials/umls/umls1.rel", delimiter="\t", encoding="GB2312", names=["CUI1", "CUI2", "REL"])
+umls_rel = pd.read_csv("../materials/umls/umlsrel_all.csv", delimiter="\t",
+                       encoding="GB2312", names=["CUI1", "CUI2", "REL"])
+umls_rel = umls_rel[umls_rel["REL"] != "SY"]
 umls_rel = umls_rel[["CUI1", "CUI2"]].applymap(lambda x: int(x[1:]))
-cuis = list(pd.read_csv("../materials/umls/cuis_all.csv", names=["CUI"])["CUI"])
-umls_rel = umls_rel[(umls_rel["CUI1"].isin(cuis)) & (umls_rel["CUI2"].isin(cuis))]
+# cuis = list(pd.read_csv("../materials/umls/cuis_all.csv", names=["CUI"])["CUI"])
+# umls_rel = umls_rel.groupby("REL")
+# umls_rel = umls_rel[(umls_rel["CUI1"].isin(cuis)) & (umls_rel["CUI2"].isin(cuis))]
 umls_rel["weight"] = 1
 umls_rel1 = umls_rel[["CUI1", "CUI2", "weight"]]
+umls_rel2 = umls_rel[["CUI1", "CUI2"]]
 umls_rel1.to_csv("../materials/umls/umls_rel_new.csv", sep="\t", encoding="utf-8", header=None, index=False)
-umls_rel.to_csv("../materials/umls/umls_rel_2.csv", sep="\t", encoding="utf-8", header=None, index=False)
+umls_rel2.to_csv("../materials/umls/umls_rel_2.csv", sep="\t", encoding="utf-8", header=None, index=False)
 print("done")
