557487d3c3311b70c124728e325b552973e67717
wen-fei <282699766@qq.com>
2020-02-11 18:57:03 +0800

    feat(process_data_index.py): function format code and add umls process code 为了方便处理，将不同知识库代码用函数分开

--- utils/process_data_index.py
+++ utils/process_data_index.py
@@ -12,13 +12,28 @@ import numpy as np
 transform data from node1, node2 to node1_index, node2_index
 """
 
-data = pd.read_csv("G:/CEGNN/utils/drugbank_d2d.cites", delimiter=" ",
-                   encoding="utf-8", names=["node_1", "node_2"])
-node_1 = data["node_1"].values.tolist()
-node_2 = data["node_2"].values.tolist()
-all = set(node_1) | set(node_2)
-nodes = pd.Series(list(all))
-index_node = pd.DataFrame()
-index_node["index"] = pd.Series(nodes.index.tolist())
-index_node["node"] = nodes
-index_node.to_csv("drugbank_index2id_edges.csv", sep="\t", encoding="utf-8", header=None, index=False)
+
+def drugbank():
+    data = pd.read_csv("G:\\CEGNN\\materials\\umls\\umls_rel_RLRQSIBCHD.csv", delimiter=" ",
+                       encoding="utf-8", names=["node_1", "node_2"])
+    node_1 = data["node_1"].values.tolist()
+    node_2 = data["node_2"].values.tolist()
+    all = set(node_1) | set(node_2)
+    nodes = pd.Series(list(all))
+    index_node = pd.DataFrame()
+    index_node["index"] = pd.Series(nodes.index.tolist())
+    index_node["node"] = nodes
+    index_node.to_csv("drugbank_index2id_edges.csv", sep="\t", encoding="utf-8", header=None, index=False)
+
+
+def umls():
+    data = pd.read_csv("G:\\CEGNN\\materials\\umls\\cuis_all.csv", delimiter=" ",
+                       encoding="utf-8", names=["node_1"])
+    nodes = data["node_1"].apply(lambda x: int(x[1:]))
+    data["index"] = nodes.index
+    data["node_1"] = nodes
+    data = data[["index", "node_1"]]
+    data.to_csv("umls_index2id_edges.csv", sep="\t", encoding="utf-8", header=None, index=False)
+
+
+umls()
