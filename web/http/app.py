30eebeb100fbe98873893d3557b30df4eadd124f
wen-fei <282699766@qq.com>
2020-03-01 21:10:54 +0800

    feat(web/http/app.py): charge the next url is right

--- web/http/app.py
+++ web/http/app.py
@@ -7,6 +7,7 @@
 """
 
 from flask import Flask, request, redirect, url_for, abort, make_response, json, jsonify
+from urllib.parse import urlparse, urljoin
 
 app = Flask(__name__)
 
@@ -97,7 +98,9 @@ def hello4():
 # 重定向到上一个页面
 def redirect_back(default="hello", **kwargs):
     for target in request.args.get("next"), request.referrer:
-        if target:
+        if not target:
+            continue
+        if is_safe_url(target):
             return redirect(target)
     return redirect(url_for(default, **kwargs))
 
@@ -106,3 +109,9 @@ def redirect_back(default="hello", **kwargs):
 def do_something():
     # do something
     return redirect_back()
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
