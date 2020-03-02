db163cd499ccad32b41ca9bab9902c10ac7c9e89
wen-fei <282699766@qq.com>
2020-03-02 09:57:24 +0800

    feat(hello/app.py): render template

--- web/hello/app.py
+++ web/hello/app.py
@@ -6,14 +6,14 @@
 @contact: tenyun.zhang.cs@gmail.com
 """
 import click
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>hello flask!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/hi")
@@ -37,3 +37,26 @@ def greet2(name):
 @app.cli.command()
 def hello():
     click.echo("hello, Human!")
+
+
+user = {
+    'username': 'Grey Li',
+    'bio': 'A boy who loves movies and music.',
+}
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'},
+]
+
+
+@app.route("/watchlist")
+def watchlist():
+    return render_template("watchlist.html", user=user, movies=movies)
