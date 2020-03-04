1342506bae6db65556282e5eb05afeacf97e3a08
wen-fei <282699766@qq.com>
2020-03-04 22:38:46 +0800

    feat(hello/app.py): add file upload function

--- web/hello/app.py
+++ web/hello/app.py
@@ -5,11 +5,55 @@
 @time: 2020/2/29
 @contact: tenyun.zhang.cs@gmail.com
 """
+import os
+
 import click
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory
+from werkzeug.utils import secure_filename
+
 from forms import LoginForm
 
 app = Flask(__name__)
+
+# file store location
+UPLOAD_FOLDER = 'G://'
+# file type allowed
+ALLOWED_EXTENSIONS = set(['pdf', "PDF"])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    """
+    check file type
+    :param filename
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    """
+    upload pdf file and store it.
+    """
+    if request.method == "POST":
+        file = request.files['pdf']
+        print(file.filename)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return url_for('page_not_found')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    """
+    show file content
+    :param filename
+    """
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 @app.route("/login")
@@ -22,84 +66,6 @@ def basic():
 def index():
     return render_template("index.html")
 
-
-@app.route("/hi")
-@app.route("/hello")
-def say_hello():
-    return "<h1>hello</h1>"
-
-
-# 动态路由
-@app.route("/greet/<name>")
-def greet(name):
-    return "<h1>Hello, %s</h1>" % name
-
-
-@app.route("/greet", defaults={"name": "Programmer"})
-@app.route("/greet/<name>")
-def greet2(name):
-    return "<h1>Hello, %s</h1>" % name
-
-
-@app.cli.command()
-def hello():
-    click.echo("hello, Human!")
-
-
-user = {
-    'username': 'Grey Li',
-    'bio': 'A boy who loves movies and music.',
-}
-movies = [
-    {'name': 'My Neighbor Totoro', 'year': '1988'},
-    {'name': 'Three Colours trilogy', 'year': '1993'},
-    {'name': 'Forrest Gump', 'year': '1994'},
-    {'name': 'Perfect Blue', 'year': '1997'},
-    {'name': 'The Matrix', 'year': '1999'},
-    {'name': 'Memento', 'year': '2000'},
-    {'name': 'The Bucket list', 'year': '2007'},
-    {'name': 'Black Swan', 'year': '2010'},
-    {'name': 'Gone Girl', 'year': '2014'},
-    {'name': 'CoCo', 'year': '2017'},
-]
-
-
-@app.route("/watchlist")
-def watchlist():
-    return render_template("watchlist.html", user=user, movies=movies)
-
-
-def inject_foo():
-    """
-    当我们调用render_template 函数渲染任意一个模板时， 所有使用
-    app.context_processor装饰器注册的模板上下文处理函数（ 包括Flask内置的上
-    下文处理函数） 都会被执行， 这些函数的返回值会被添加到模板中， 因此我们可以
-    在模板中直接使用foo变量。
-    :return:
-    """
-    foo = "i am foo."
-    # equal to return {'foo': foo}
-    return dict(foo=foo)
-
-
-# 自定义上下文
-app.context_processor(inject_foo)
-
-
-@app.template_global()
-def bar():
-    return "I am a bar"
-
-
-app.jinja_env.globals["bar"] = bar
-app.jinja_env.globals["user"] = user
-
-
-def smiling(s):
-    return s + " :) "
-
-
-app.jinja_env.filters["smiling"] = smiling
 
 app.secret_key = 'secret string'
 
@@ -114,3 +80,7 @@ def just_flash():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
