5fb746785bbc6533b78516da13f2860f5ede33bf
wen-fei <282699766@qq.com>
2020-04-04 20:23:05 +0800

    add lr

--- experiments/node_classfication/LR.py
+++ experiments/node_classfication/LR.py
@@ -8,6 +8,8 @@
 import logging
 import os
 import time
+from collections import Counter
+
 import gensim
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
@@ -20,53 +22,119 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 
-def load_embeddings(model, nodes_list, embedding_file, dim=200):
+def load_embeddings(nodes_list, embedding_file, dim=200):
+    """
+    针对gensim Word2vec模型保存的embedding文件进行读取
+    """
     embeddings_model = gensim.models.KeyedVectors.load_word2vec_format(embedding_file, binary=False,
                                                                        unicode_errors='ignore')
-    logging.info("load {} embedding...".format(embedding_file))
     tic = time.time()
-    logging.info('{} load done.  (time used: {:.1f}s)\n'.format(model, time.time() - tic))
+    # print('{} load done.  (time used: {:.1f}s)\n'.format(model, time.time() - tic))
     embedding_weights = []
     found = 0
     notfound = 0
-    logging.info("start index word include embedding vector and store...")
-    for node in tqdm(nodes_list):
-        if id in embeddings_model.vocab:
-            embedding_weights.append(embeddings_model.word_vec(node[2:]))
+    # print("start index word include embedding vector and store...")
+    for node in nodes_list:
+        node = int(node[2:])
+        if str(node) in embeddings_model.vocab:
+            embedding_weights.append(embeddings_model.word_vec(str(node)))
             found += 1
         else:
             embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
             notfound += 1
-    logging.info("found_cnt size is :" + str(found))
-    logging.info("not found_cnt size is :" + str(notfound))
-    logging.info("embedding_weights size is %s " % (len(embedding_weights)))
+    # print("found_cnt size is :" + str(found))
+    # print("not found_cnt size is :" + str(notfound))
+    # print("embedding_weights size is %s " % (len(embedding_weights)))
     return embedding_weights
 
 
-def node_classification(dataset_path, dataset, model, emb_path, embedding, dim=200):
-    ids_labels = np.genfromtxt(os.path.join(dataset_path, dataset), delimiter=",", dtype=np.dtype(str))
-    nodes = ids_labels[:, 0]
-    labels = ids_labels[:, -1]
-    features = load_embeddings(model, nodes, os.path.join(emb_path, embedding), dim)
+def load_embedding_asne(nodes_list, embedding_file, dim=200):
+    """
+    针对asne模型的输出文件读取embedding
+    """
+    emb = pd.read_csv(embedding_file, index_col=False)
+    n2i = {}
+    index2node = pd.read_csv("G:\\CEGNN\\materials\\drugbank\\drugbank_index2id_edges.csv",
+                             delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
+    for index, row in index2node.iterrows():
+        n2i[row["node"]] = row["index"]
+    embedding_weights = []
+    emb["id"] = emb["id"].apply(lambda x: str(int(x)))
+    indexes = {x: 1 for x in list(emb["id"])}
+    for node in nodes_list:
+        node = str(int(node[2:]))
+        if node in n2i:
+            if n2i[node] in indexes:
+                vector = emb[emb["id"] == n2i[node]].values.tolist()[0][1:]
+                embedding_weights.append(vector)
+            else:
+                embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
+        else:
+            embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
+    return embedding_weights
 
-    lr = LogisticRegression()
-    clf = svm.SVC(decision_function_shape='ovo')
-    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=0)
-    clf.fit(X=x_train, y=y_train)
-    y_predict = clf.predict(x_test)
-    precision = precision_score(y_test, y_predict, average=None)
-    recall = recall_score(y_test, y_predict, average=None)
-    f1 = f1_score(y_test, y_predict, average=None)
-    micro_f1 = f1_score(y_true=y_test, y_pred=y_predict, labels=list(set(labels)), average="micro")
-    macro_f1 = f1_score(y_true=y_test, y_pred=y_predict, labels=list(set(labels)), average="macro")
-    now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    result = "{} : the model of {} node classification " \
-             "experiment result is : " \
-             "P:{} R:{} F1:{} Micro_F1: {} Macro_F1: {}".format(now, dataset, precision,
-                                                                recall, f1, micro_f1, macro_f1)
+
+def load_embedding_gemsec(nodes_list, embedding_file, dim=200):
+    """
+    针对gemsec模型的输出文件读取embedding
+    """
+    emb = pd.read_csv(embedding_file, index_col=False)
+    n2i = {}
+    index2node = pd.read_csv("G:\\CEGNN\\materials\\drugbank\\drugbank_index2id_edges.csv",
+                             delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
+    for index, row in index2node.iterrows():
+        n2i[row["node"]] = row["index"]
+    embedding_weights = []
+    # emb["id"] = emb["id"].apply(lambda x: str(int(x)))
+    indexes = {str(x): 1 for x in list(emb.index)}
+    for node in nodes_list:
+        node = str(int(node[2:]))
+        if node in n2i:
+            if n2i[node] in indexes:
+                vector = emb.iloc[int(n2i[node])].tolist()
+                embedding_weights.append(vector)
+            else:
+                embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
+        else:
+            embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
+    return embedding_weights
+
+
+def node_classification(model, emb_path, embedding, dim=200):
+    if model == "ASNE":
+        features = load_embedding_asne(nodes, os.path.join(emb_path, embedding), dim)
+    elif model == "GEMSEC":
+        features = load_embedding_gemsec(nodes, os.path.join(emb_path, embedding), dim)
+    else:
+        features = load_embeddings(nodes, os.path.join(emb_path, embedding), dim)
+    results = []
+    for labeled_part in np.arange(0.3, 0.9, 0.3):
+        labeled_part = round(labeled_part, 1)
+        clf = LogisticRegression()
+        # clf = svm.SVR(decision_function_shape='ovo')
+        # clf = svm.LinearSVR()
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=labeled_part, random_state=0)
+        clf.fit(X=x_train, y=y_train)
+        y_predict = clf.predict(x_test)
+        precision = precision_score(y_test, y_predict, average="weighted")
+        recall = recall_score(y_test, y_predict, average="weighted")
+        f1 = f1_score(y_test, y_predict, average="weighted")
+        micro_f1 = f1_score(y_true=y_test, y_pred=y_predict, labels=list(set(labels)), average="micro")
+        macro_f1 = f1_score(y_true=y_test, y_pred=y_predict, labels=list(set(labels)), average="macro")
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        result = "{} : model: {} node-classification : " \
+                 "labeled nodes(%): {} " \
+                 "P:{:.4f} R:{:.4f} F1:{:.4f} " \
+                 "Micro_F1: {:.4f} " \
+                 "Macro_F1: {:.4f}".format(now, model_name, str(labeled_part * 100) + "%",
+                                           precision, recall, f1, micro_f1,
+                                           macro_f1)
+        results.append(result)
     with open("node_classification.result", "a+") as f:
-        f.write(result)
-        f.write("\r\n")
+        for line in results:
+            f.write(line)
+            f.write("\n")
+        f.write("\n")
 
 
 if __name__ == '__main__':
@@ -74,14 +142,30 @@ if __name__ == '__main__':
     dataset_path = "G:\\CEGNN\\materials\\drugbank"
     # emb_path = "/home/gp/stu/ztt/data/embs/drugbank/"
     emb_path = "G:\\CEGNN\\materials\\emb\\drugbank"
-    dataset = "drugbank_labels.csv"
+    # Counter({'solid': 2200, 'None': 1496, 'liquid': 284, 'gas': 8})
+    dataset = "drugsbank_action_labels.csv"
     models = {
-        # "ASNE": "drugbank_asne_200.emb",
+        "ASNE": "drugbank_asne_200.emb",
         "deepwalk": "drugbank_deepwalk_200.embeddings",
-        # "GEMSEC": "drugbank_GEMSEC_200.emb",
+        "GEMSEC": "drugbank_GEMSEC_200.emb",
         "node2vec": "drugbank_node2vec_200.emb",
         "struct2vec": "drugbank_struct2vec_200.emb",
         "LINE": "vec_drugbank_all.txt"
     }
+
+    ids_labels = pd.read_csv(os.path.join(dataset_path, dataset), delimiter=",", dtype=np.dtype(str),
+                             names=["ids", "labels"])
+    # ids_labels = ids_labels[ids_labels["labels"].isin(["inhibitor", "antagonist", "agonist", "binder"])]
+    ids_labels = ids_labels[ids_labels["labels"] != "None"]
+    # ", "antibody",
+    # "potentiator", "activator", "ligand"])]
+    # ids_labels = ids_labels[np.where(ids_labels[:, -1] != "gas")]
+    nodes = ids_labels["ids"].tolist()
+    labels = ids_labels["labels"].tolist()
+    s = Counter(labels)
+    print(s)
     for model_name, embedding in tqdm(models.items()):
-        node_classification(dataset_path, dataset, model_name, emb_path, embedding)
+        if model_name == "LINE":
+            node_classification(model_name, emb_path, embedding, dim=400)
+        else:
+            node_classification(model_name, emb_path, embedding)
