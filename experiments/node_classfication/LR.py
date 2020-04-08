268af3ddf87341a187c961637eb8621ee41a125f
wen-fei <282699766@qq.com>
2020-04-08 23:25:14 +0800

    更新参数

--- experiments/node_classfication/LR.py
+++ experiments/node_classfication/LR.py
@@ -13,6 +13,7 @@ from collections import Counter
 import gensim
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVR
 import pandas as pd
 import numpy as np
@@ -20,6 +21,9 @@ import scipy.sparse as sp
 from sklearn.metrics import f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def load_embeddings(nodes_list, embedding_file, dim=200):
@@ -39,11 +43,12 @@ def load_embeddings(nodes_list, embedding_file, dim=200):
         if str(node) in embeddings_model.vocab:
             embedding_weights.append(embeddings_model.word_vec(str(node)))
             found += 1
+            # print(node)
         else:
             embedding_weights.append(np.random.uniform(-0.25, 0.25, dim).astype(np.float32))
             notfound += 1
     # print("found_cnt size is :" + str(found))
-    # print("not found_cnt size is :" + str(notfound))
+    print("not found_cnt size is :" + str(notfound))
     # print("embedding_weights size is %s " % (len(embedding_weights)))
     return embedding_weights
 
@@ -54,8 +59,7 @@ def load_embedding_asne(nodes_list, embedding_file, dim=200):
     """
     emb = pd.read_csv(embedding_file, index_col=False)
     n2i = {}
-    index2node = pd.read_csv("G:\\CEGNN\\materials\\drugbank\\drugbank_index2id_edges.csv",
-                             delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
+    index2node = pd.read_csv(inv_path, delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
     for index, row in index2node.iterrows():
         n2i[row["node"]] = row["index"]
     embedding_weights = []
@@ -80,8 +84,7 @@ def load_embedding_gemsec(nodes_list, embedding_file, dim=200):
     """
     emb = pd.read_csv(embedding_file, index_col=False)
     n2i = {}
-    index2node = pd.read_csv("G:\\CEGNN\\materials\\drugbank\\drugbank_index2id_edges.csv",
-                             delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
+    index2node = pd.read_csv(inv_path, delimiter="\t", encoding="utf-8", names=["index", "node"], dtype=str)
     for index, row in index2node.iterrows():
         n2i[row["node"]] = row["index"]
     embedding_weights = []
@@ -108,12 +111,13 @@ def node_classification(model, emb_path, embedding, dim=200):
     else:
         features = load_embeddings(nodes, os.path.join(emb_path, embedding), dim)
     results = []
-    for labeled_part in np.arange(0.3, 0.9, 0.3):
+    for labeled_part in np.arange(0.1, 1.0, 0.1):
         labeled_part = round(labeled_part, 1)
         clf = LogisticRegression()
         # clf = svm.SVR(decision_function_shape='ovo')
         # clf = svm.LinearSVR()
-        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=labeled_part, random_state=0)
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, shuffle=True, test_size=labeled_part,
+                                                            random_state=0)
         clf.fit(X=x_train, y=y_train)
         y_predict = clf.predict(x_test)
         precision = precision_score(y_test, y_predict, average="weighted")
@@ -126,7 +130,7 @@ def node_classification(model, emb_path, embedding, dim=200):
                  "labeled nodes(%): {} " \
                  "P:{:.4f} R:{:.4f} F1:{:.4f} " \
                  "Micro_F1: {:.4f} " \
-                 "Macro_F1: {:.4f}".format(now, model_name, str(labeled_part * 100) + "%",
+                 "Macro_F1: {:.4f}".format(now, model_name, str(100 - labeled_part * 100) + "%",
                                            precision, recall, f1, micro_f1,
                                            macro_f1)
         results.append(result)
@@ -139,33 +143,35 @@ def node_classification(model, emb_path, embedding, dim=200):
 
 if __name__ == '__main__':
     # dataset_path = "/home/gp/stu/ztt/data/drugbank/"
-    dataset_path = "G:\\CEGNN\\materials\\umls"
+    dataset_path = "G:\\CEGNN\\materials\\drugbank"
     # emb_path = "/home/gp/stu/ztt/data/embs/drugbank/"
-    emb_path = "G:\\CEGNN\\materials\\emb\\umls"
+    emb_path = "G:\\CEGNN\\materials\\emb\\drugbank"
+    inv_path = "G:\\CEGNN\\materials\\drugbank\\drugbank_index2id_edges.csv"
     # Counter({'solid': 2200, 'None': 1496, 'liquid': 284, 'gas': 8})
-    # dataset = "drugsbank_action_labels.csv"
-    dataset = "umls_sty_samples.csv"
-    drugbank_models = {
-        "ASNE": "drugbank_asne_200.emb",
-        "deepwalk": "drugbank_deepwalk_200.embeddings",
-        "GEMSEC": "drugbank_GEMSEC_200.emb",
-        "node2vec": "drugbank_node2vec_200.emb",
-        "struct2vec": "drugbank_struct2vec_200.emb",
-        "LINE": "vec_drugbank_all.txt"
+    dataset = "drugbank_action_labels.csv"
+    # dataset = "umls_sty_samples.csv"
+    dataset_prefix = "drugbank"
+    models = {
+        "ASNE": dataset_prefix + "_asne_200.emb",
+        "deepwalk": dataset_prefix + "_deepwalk_200.emb",
+        "GEMSEC": dataset_prefix + "_GEMSEC_200.emb",
+        "node2vec": dataset_prefix + "_node2vec_200.emb",
+        "struct2vec": dataset_prefix + "_struct2vec_200.emb",
+        "LINE": "vec_" + dataset_prefix + "_all.txt"
     }
-
-    umls_models = {
-        "deepwalk": "umls_deepwalk_200.embeddings",
-    }
-
     ids_labels = pd.read_csv(os.path.join(dataset_path, dataset), delimiter=",", dtype=np.dtype(str),
                              names=["ids", "labels"])
-    ids_labels = ids_labels[ids_labels["labels"] != "None"]
+    ids_labels = ids_labels[ids_labels["labels"].isin(["inhibitor", "antagonist", "agonist"])]
     nodes = ids_labels["ids"].tolist()
     labels = ids_labels["labels"].tolist()
-    # s = Counter(labels)
-    for model_name, embedding in tqdm(drugbank_models.items()):
+    s = Counter(labels)
+    print(s)
+    lb = LabelEncoder()
+    lb.fit(labels)
+    labels = lb.fit_transform(labels)
+
+    for model_name, embedding in models.items():
         if model_name == "LINE":
             node_classification(model_name, emb_path, embedding, dim=400)
         else:
-            node_classification(model_name, emb_path, embedding)
+            node_classification(model_name, emb_path, embedding, dim=200)
