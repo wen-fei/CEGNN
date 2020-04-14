5aa55b05d782ec962fb7f177092a2066108145dc
wen-fei <282699766@qq.com>
2020-04-14 22:41:18 +0800

    test file

---
+++ experiments/node_classfication/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:TenYun
+@time: 2020/4/4
+@contact: tenyun.zhang.cs@gmail.com
+"""
+import os
+
+import numpy as np
+import pandas as pd
+
+for i in np.arange(0.1, 0.9, 0.1):
+    print(i)
