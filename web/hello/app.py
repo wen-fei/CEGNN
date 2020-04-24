ed71aaf6246ec57fc2cfb1e5a4304c9cb6b80428
wen-fei <282699766@qq.com>
2020-04-24 11:09:31 +0800

    fix upload file function and add read abstract result func

--- web/hello/app.py
+++ web/hello/app.py
@@ -6,20 +6,25 @@
 @contact: tenyun.zhang.cs@gmail.com
 """
 import os
+import time
 
 import click
-from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory
+from flask import Flask, render_template, flash, redirect, \
+    url_for, request, send_from_directory, g, jsonify
 from werkzeug.utils import secure_filename
 
+from extractPDF import ExtractPDF
 from forms import LoginForm
 
 app = Flask(__name__)
 
 # file store location
-UPLOAD_FOLDER = 'G://'
+UPLOAD_FOLDER = 'G:\\CEGNN\\web\\hello\\resource\\'
 # file type allowed
 ALLOWED_EXTENSIONS = set(['pdf', "PDF"])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER + "\\pdfs"
+app.config['docs_pkl'] = "G:\\CEGNN\\web\\hello\\resource\\docs.pkl"
+app.config["abstract_result"] = UPLOAD_FOLDER + "\\abstracts"
 
 
 def allowed_file(filename):
@@ -31,25 +36,53 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
+def read_abstract(path):
+    """
+    read abstract result
+    """
+    files = os.listdir(path)
+    result = {}
+    for file in files:
+        with open(file) as f:
+            result["file"] = f.read()
+    return result
+
+
 @app.route("/upload", methods=["GET", "POST"])
 def upload_file():
     """
     upload pdf file and store it.
+    # TODO: parse multi files
     """
+    extractPDF = ExtractPDF()
+    response = {}
     if request.method == "POST":
         file = request.files['pdf']
-        print(file.filename)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return url_for('page_not_found')
+            # save file
+            now = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+            pdf_path = app.config['UPLOAD_FOLDER'] + "/" + now
+            abstract_result_path = app.config['abstract_result'] + "/" + now
+            os.mkdir(pdf_path)
+            os.mkdir(abstract_result_path)
+            path = os.path.join(pdf_path, filename)
+            file.save(path)
+            # parse pdf file
+            extractPDF.parse(pdf_path, abstract_result_path, app.config['docs_pkl'])
+            abstract_result = read_abstract(os.listdir(abstract_result_path))
+            response["msg"] = abstract_result
+            # return redirect(url_for('uploaded_file', filename=filename))
+        else:
+            response["ok"] = "False"
+            response["msg"] = "file is invalid. please re-upload pdf file."
+    return response
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     """
-    show file content
+    show medical abstract content
     :param filename
     """
     return send_from_directory(app.config['UPLOAD_FOLDER'],
